0e2ce847145e3e147a56f3701c38418e90f0edbc
Mouawya <mouawya88@gmail.com>
2021-08-01 19:24:17 +0200

    refactored and modified

--- Week1/create-meetup.js
+++ Week1/create-meetup.js
@@ -9,87 +9,73 @@ const connection = mysql.createConnection({
 
 //Connecting with mysql
 connection.connect((err) => {
-  if (err) throw err;
+  if (err) console.log(err);
   console.log("Connected!");
 });
 
-connection.query("DROP DATABASE meetup");
-
-//Creating meetup database
-connection.query("CREATE DATABASE meetup", (err, res) => {
-  if (err) throw err;
-  console.log("Database created!");
-});
-
-connection.query("USE meetup", (err) => {
-  if (err) throw err;
-  console.log("meetup selected!");
-});
+//sql function
+const sql = (query, consoleMessage, values) => {
+  connection.query(query, [values], (err, result) => {
+    if (err) console.log(err);
+    console.log(consoleMessage);
+  });
+};
 
 //Creating invitee table
 const invitee =
-  "CREATE TABLE invitee(invitee_no INT, invitee_name TEXT, invited_by TEXT)";
-connection.query(invitee, (err, result) => {
-  if (err) throw err;
-  console.log("Invitee table created!");
-});
+  "CREATE TABLE invitee(invitee_no INT, invitee_name VARCHAR(50), invited_by VARCHAR(50))";
 
 //Creating room table
 const room =
-  "CREATE TABLE room(room_no INT, room_name TEXT, floor_number TEXT)";
-connection.query(room, (err, result) => {
-  if (err) throw err;
-  console.log("Room table created!");
-});
+  "CREATE TABLE room(room_no INT, room_name VARCHAR(50), floor_number INT)";
 
 //Creating meeting table
 const meeting =
-  "CREATE TABLE meeting(meeting_no INT, meeting_title TEXT, starting_time TIME, ending_time TIME, room_no INT)";
-connection.query(meeting, (err, result) => {
-  if (err) throw err;
-  console.log("Meeting table created!");
-});
+  "CREATE TABLE meeting(meeting_no INT, meeting_title TEXT, starting_time DATETIME, ending_time DATETIME, room_no INT)";
 
 //Inserting rows into invitee table
-const sql =
+const insertQueryInvitee =
   "INSERT INTO invitee(invitee_no, invitee_name, invited_by) VALUES ?";
-const values = [
+const inviteeValues = [
   [1, "John", "Ali"],
   [2, "Mo", "Ali"],
   [3, "Steve", "Ali"],
   [4, "Nour", "Ali"],
   [5, "Samer", "Ali"],
 ];
-connection.query(sql, [values], (err, result) => {
-  if (err) throw err;
-  console.log("records inserted!");
-});
 
 //Inserting rows into room table
-const sql2 = "INSERT INTO room(room_no, room_name, floor_number) VALUES ?";
-const values2 = [
-  [75, "The Capital City", "1st"],
-  [12, "Collective IQ", "1st"],
-  [84, "Gig Gallery", "3d"],
-  [23, "Alpha Room", "2d"],
-  [45, "Fellowship Hall", "3d"],
+const insertQueryRoom =
+  "INSERT INTO room(room_no, room_name, floor_number) VALUES ?";
+const roomValues = [
+  [75, "The Capital City", 1],
+  [12, "Collective IQ", 1],
+  [84, "Gig Gallery", 3],
+  [23, "Alpha Room", 2],
+  [45, "Fellowship Hall", 3],
 ];
-connection.query(sql2, [values2], (err, result) => {
-  if (err) throw err;
-  console.log("records inserted!");
-});
 
 //Inserting rows into meeting table
-const sql3 =
+const insertQueryMeeting =
   "INSERT INTO meeting(meeting_no, meeting_title, starting_time, ending_time, room_no) VALUES ?";
-const values3 = [
-  [5, "Status Update", "12:00", "13:00", 75],
-  [6, "Information Sharing ", "12:00", "13:00", 84],
-  [7, "Decision Making", "13:00", "14:00", 23],
-  [8, "Problem Solving ", "13:00", "14:00", 45],
-  [9, "Innovation", "12:00", "13:00", 12],
+const meetingValues = [
+  [5, "Status Update", "2021-09-05:12:00", "2021-09-05:13:00", 75],
+  [6, "Information Sharing ", "2021-09-05:12:00", "2021-09-05:13:00", 84],
+  [7, "Decision Making", "2021-09-05:13:00", "2021-09-05:14:00", 23],
+  [8, "Problem Solving ", "2021-09-05:13:00", "2021-09-05:14:00", 45],
+  [9, "Innovation", "2021-09-05:12:00", "2021-09-05:13:00", 12],
 ];
-connection.query(sql3, [values3], (err, result) => {
-  if (err) throw err;
-  console.log("records inserted!");
-});
+
+sql("DROP DATABASE IF EXISTS meetup");
+sql("CREATE DATABASE meetup", "Database created!");
+sql("USE meetup", "meetup selected!");
+
+sql(invitee, "invitee table created!");
+sql(room, "room table created!");
+sql(meeting, "meeting table created!");
+
+sql(insertQueryInvitee, "record inserted!", inviteeValues);
+sql(insertQueryRoom, "record inserted!", roomValues);
+sql(insertQueryMeeting, "record inserted!", meetingValues);
+
+connection.end();
--- Week1/world.js
+++ Week1/world.js
@@ -9,102 +9,82 @@ const connection = mysql.createConnection({
 });
 
 connection.connect((err) => {
-  if (err) throw err;
+  if (err) console.log(err);
 });
 
+//sql function
+const sql = (query, consoleMessage) => {
+  connection.query(query, (err, result) => {
+    if (err) console.log(err);
+    console.log(consoleMessage);
+    result.forEach((obj) => console.log(obj.name));
+  });
+};
+
 //Question 1:
-connection.query(
+sql(
   "SELECT name FROM country WHERE population > 8000000",
-  (err, result) => {
-    if (err) throw err;
-    console.log("Countries with population greater than 8 million:");
-    result.forEach((obj) => console.log(obj.name));
-  }
+  "Countries with population greater than 8 million:"
 );
+
 //Question 2:
-connection.query(
+sql(
   "SELECT name FROM country WHERE name LIKE '%land%' ",
-  (err, result) => {
-    if (err) throw err;
-    console.log("Countries that have “land” in their names:");
-    result.forEach((obj) => console.log(obj.name));
-  }
+  "Countries that have “land” in their names:"
 );
+
 //Question 3:
-connection.query(
+sql(
   "SELECT name FROM city WHERE population BETWEEN 500000 AND 1000000",
-  (err, result) => {
-    if (err) throw err;
-    console.log("Cities with population in between 500,000 and 1 million:");
-    result.forEach((obj) => console.log(obj.name));
-  }
+  "Cities with population in between 500,000 and 1 million:"
 );
+
 //Question 4:
-connection.query(
+sql(
   "SELECT name FROM country WHERE continent = 'Europe'",
-  (err, result) => {
-    if (err) throw err;
-    console.log("All the countries on the continent ‘Europe’:");
-    result.forEach((obj) => console.log(obj.name));
-  }
+  "All the countries on the continent ‘Europe’:"
 );
 
 //Question 5:
-connection.query(
-  "SELECT name FROM country ORDER BY surfaceArea DESC ",
-  (err, result) => {
-    if (err) throw err;
-    console.log(
-      "All the countries in the descending order of their surface areas:"
-    );
-    result.forEach((obj) => console.log(obj.name));
-  }
+sql(
+  "SELECT name FROM country ORDER BY surfaceArea DESC",
+  "All the countries in the descending order of their surface areas:"
 );
 
 //Question 6:
-connection.query(
+sql(
   "SELECT name FROM city WHERE countryCode = 'NLD' ",
-  (err, result) => {
-    if (err) throw err;
-    console.log("All the cities in the Netherlands:");
-    result.forEach((obj) => console.log(obj.name));
-  }
+  "All the cities in the Netherlands:"
 );
 
 //Question 7:
 connection.query(
   "SELECT population FROM city WHERE name = 'Rotterdam' ",
   (err, result) => {
-    if (err) throw err;
+    if (err) console.log(err);
     console.log("The population of Rotterdam:");
     result.forEach((obj) => console.log(obj.population));
   }
 );
 
 //Question 8:
-connection.query(
-  "SELECT name FROM country ORDER BY surfaceArea DESC LIMIT 10 ",
-  (err, result) => {
-    if (err) throw err;
-    console.log("The top 10 countries by Surface Area:");
-    result.forEach((obj) => console.log(obj.name));
-  }
+sql(
+  "SELECT name FROM country ORDER BY surfaceArea DESC LIMIT 10",
+  "The top 10 countries by Surface Area:"
 );
 
 //Question 9:
-connection.query(
+sql(
   "SELECT name FROM city ORDER BY population DESC LIMIT 10 ",
-  (err, result) => {
-    if (err) throw err;
-    console.log("The top 10 most populated cities:");
-    result.forEach((obj) => console.log(obj.name));
-  }
+  "The top 10 most populated cities:"
 );
 
 //Question 10:
 connection.query("SELECT SUM(population) FROM country", (err, result) => {
-  if (err) throw err;
+  if (err) console.log(err);
   console.log(
     `The population number of the world: ${Object.values(result[0])[0]}`
   );
 });
+
+connection.end();
